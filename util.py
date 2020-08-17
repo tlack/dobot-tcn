4dc83d6a0b2177baede5a5488198314e877dd63a
tlack <lackner@gmail.com>
2020-08-16 23:38:21 -0400

    util.py: pretty() vector formatter

--- util.py
+++ util.py
@@ -8,5 +8,16 @@ def emit(x, t):
 def noemit(x, t):
     return x
 
+def pretty(float_vec):
+    if type(float_vec) == type({}):
+        float_vec = float_vec.values()
+    # emit(float_vec,'pretty')
+    def pad(x): 
+        n = 8
+        while len(x) < n: 
+            x = " "+x
+        return x
+    return ','.join([pad(str('%0.4f' % x)) for x in float_vec])
+
 def timestamp():
     return datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d%H%M%S')
